4a52fc369ae76d4470b9ffcf19ae3016373b419e
kdrkdrkdr <kdrhacker1234@gmail.com>
2022-06-06 01:40:43 +0900

    add files

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from detector import Detector
 
 d = Detector('weights.h5')
 d.run_on_folder(
-    input_folder='/content/gray_scale/',
-    output_folder='/content/gray_scale_detected', 
+    input_folder='/content/image_original/',
+    output_folder='/content/image_detected/', 
     is_video=False, 
     is_mosaic=False,
     dilation=3
