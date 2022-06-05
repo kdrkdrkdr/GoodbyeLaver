39ebd216ce0ded79ba4c8b3e6a337f0f4b3e721a
kdrkdrkdr <kdrhacker1234@gmail.com>
2022-06-06 01:42:06 +0900

    edit files

--- run_lama.py
+++ run_lama.py
@@ -1,11 +1,11 @@
 import requests
 from PIL import Image
-
+from os import listdir
 
 def run_inpaint(image_path, mask_path, output_path):
     w, h = Image.open(image_path).size
     r = requests.post(
-        url = 'http://127.0.0.1:8080/inpaint',
+        url = 'http://127.0.0.1:5003/inpaint',
         files={
             'image':open(image_path, 'rb'),
             'mask':open(mask_path, 'rb'),
@@ -43,19 +43,14 @@ def make_mask(detected_image):
 
 
 if __name__ == '__main__':
-    f = 'full_color'
-    g = 'gray_scale'
 
+    for i in listdir('/content/image_detected/'):
+        mask = make_mask(f'/content/image_detected/{i}')
+        print('mask 완료')
 
-    mask = make_mask(f'./image/{g}_detected/11.png')
-    print('mask 완료')
-
-
-    run_inpaint(
-        image_path=f'./image/{g}/11.png',
-        mask_path=mask,
-        output_path='res.png'
-    )
-    print('inpaint 완료')
-    import os
-    os.system('res.png')
+        run_inpaint(
+            image_path=f'/content/image_original/{i}',
+            mask_path=mask,
+            output_path=f'/content/image_uncensored/{i}'
+        )
+        print('inpaint 완료')
